c407ae41423df12b4ca7b86aaada7b968d698a35
spanoselias <spanoselias@outlook.com>
2017-02-13 09:31:09 -0800

    changeLoadScript

--- loadAll.sh
+++ loadAll.sh
@@ -5,30 +5,17 @@
 mysql  < drop.sql
 echo "All the tables are dropped"
 
-
 mysql < create_new.sql
 echo "Database is created"
 
-javac src/MySAX_Mod.java
+javac MySAX_Mod.java
 echo "Java code is compiled"
 
-rm -r tmp
-echo "tmp folder is deleted if exist"
-
-mkdir tmp
-echo "tmp folder is created"
-
-cp src/MySAX_Mod.class tmp/
-
-cp -R ebay-data/items-*.xml tmp/
-cp Csvs/load.sql tmp/
-
-cd tmp/
-
-java MySAX_Mod items-*.xml
+java MySAX_Mod ebay-data/items-*.xml
 echo "Java code is executed"
 
-rm *.xml
-rm *.class
-
 mysql EBY < load.sql
+echo "Load csv files"
+
+rm *.class
+rm *.csv

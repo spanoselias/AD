24db645f7849b4d60461d13016d240d088197c3b
spanoselias <spanoselias@outlook.com>
2017-02-13 05:22:25 -0800

    commenting loadAll.sh

--- loadAll.sh
+++ loadAll.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Run the drop.sql batch file to drop existing tables.
+# Inside the drop.sql, it is checked whether the table exists. They are Dropped them ONLY if they exist.
 mysql  < drop.sql
-
 echo "All the tables are dropped"
+
 
 mysql < create_new.sql
 echo "Database is created"
@@ -11,7 +13,10 @@ javac src/MySAX_Mod.java
 echo "Java code is compiled"
 
 rm -r tmp
+echo "tmp folder is deleted if exist"
+
 mkdir tmp
+echo "tmp folder is created"
 
 cp src/MySAX_Mod.class tmp/
 

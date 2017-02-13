191ec30d7a9f53e198ef6ec347ac5e61b6862ccc
spanoselias <spanoselias@outlook.com>
2017-02-13 09:52:46 -0800

    rename script

---
+++ runLoad.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run the drop.sql batch file to drop existing tables.
+# Inside the drop.sql, it is checked whether the table exists. They are Dropped them ONLY if they exist.
+mysql  < drop.sql
+echo "All the tables are dropped"
+
+mysql < create_new.sql
+echo "Database is created"
+
+javac MySAX_Mod.java
+echo "Java code is compiled"
+
+java MySAX_Mod ebay-data/items-*.xml
+echo "Java code is executed"
+
+mysql EBY < load.sql
+echo "Load csv files"
+
+rm *.class
+rm *.csv
+
+mysql EBY < queries.sql

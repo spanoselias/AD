35776e45f33bbd87c414e5d54d9591adcf82eeef
spanoselias <spanoselias@outlook.com>
2017-02-14 06:30:59 -0800

    submitFile

---
+++ Running/drop.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS
+EBY.Bidder_Country,
+EBY.Bidder_Location,
+EBY.Bid_Act,
+EBY.Item_Category,
+EBY.Init_Price,
+EBY.Item,
+EBY.Location_Det,
+EBY.Location,
+EBY.Bidder,
+EBY.Seller,
+EBY.Country,
+EBY.Category;

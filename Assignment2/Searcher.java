1b81e9e4a3d1c719d5473e37aef0a97061b3782f
aphaea <afaias.revival@gmail.com>
2017-03-09 20:58:53 +0000

    initial searcher

--- Assignment2/Searcher.java
+++ Assignment2/Searcher.java
@@ -33,34 +33,74 @@ import org.apache.lucene.document.Field.Store;
 import java.io.*;
 import java.nio.file.Path;
 
-public class Searcher {
+public class Searcher implements Comparator<Item> {
+    public class Searcher {
 
-    public Searcher() {}
-    public static void main(String[] args) throws Exception {
-	String usage = "java Searcher";
-	search(args[0], "indexes");
-    }    
-    
-    private static TopDocs search(String searchText, String p) {   
-	System.out.println("Running search(" + searchText + ")");
-	try {   
-	    Path path = Paths.get(p);
-	    Directory directory = FSDirectory.open(path);       
-	    IndexReader indexReader =  DirectoryReader.open(directory);
-	    IndexSearcher indexSearcher = new IndexSearcher(indexReader);
-	    QueryParser queryParser = new QueryParser("line", new SimpleAnalyzer());  
-	    Query query = queryParser.parse(searchText);
-	    TopDocs topDocs = indexSearcher.search(query,10000);
-	    System.out.println("Number of Hits: " + topDocs.totalHits);
-	    for (ScoreDoc scoreDoc : topDocs.scoreDocs) {           
-		Document document = indexSearcher.doc(scoreDoc.doc);
-		System.out.println("doc_id: " + document.get("doc_id") 
-				   + ", score: " + scoreDoc.score + " [" + document.get("line") +"]");
+        public Searcher() {}
+        static double latitude,longitude,width;
+	    static Point[] box = null;
+	    static boolean basicSearch;
+	    public static void main(String[] args) throws Exception {
+		   String usage = "java Searcher";
+
+		if(args.length == 1){
+			//basicSearch = true;
+			search(args[0], "indexes");
+		}
+		else if(args.length == 7){
+			// check if we have 6 arguments
+			if( args[1].equals("-x") && args[3].equals("-y") && args[5].equals("-w") ){
+
+				latitude = Double.valueOf(args[4]);
+				longitude = Double.valueOf(args[2]);
+				width = Double.valueOf(args[6]);
+				}
+           
+        
+        private static TopDocs search(String searchText, String p) {   
+	    System.out.println("Running search(" + searchText + ")");
+	    Connection dbConn = null;
+	    
+	    try {   
+	        Path path = Paths.get(p);
+	        Directory directory = FSDirectory.open(path);       
+	        IndexReader indexReader =  DirectoryReader.open(directory);
+	        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+	        QueryParser queryParser = new QueryParser("line", new SimpleAnalyzer());  
+	        Query query = queryParser.parse(searchText);
+	        TopDocs topDocs = indexSearcher.search(query,10000);
+	        
+	        // create a connection to the database
+			dbConn = DbManager.getConnection(true);
+	        
+	        System.out.println("Number of Hits: " + topDocs.totalHits);
+	        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {           
+		    Document document = indexSearcher.doc(scoreDoc.doc);
+		    System.out.println("item_id: " + document.get("doc_id") 
+				       + ", score: " + scoreDoc.score + " [" + document.get("line") +"]");
+		    String item_id = document.get("doc_id");
+			String item_name = document.get("line");
+			float score = scoreDoc.score;
+			float current_price;
+			
+			String SQLquery = "SELECT current_price FROM auction WHERE item_id = "+item_id;
+			// get the current price for the specific item
+			State = dbConn.State(SQLquery);
+			ResultSet result = State.executeQuery();
+			result.next();
+			current_price = result.getInt("current_price");
+			
+			// create an item and add to list
+			Item item = new Item(item_id,item_name,score,current_price);
+			itemList.add(item);
+	        }
+	        System.out.println("Number of Hits: " + topDocs.totalHits);
+	        State.close();
+			dbConn.close();
+	        return topDocs;
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return null;
 	    }
-	    return topDocs;
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    return null;
-	}
+        }
     }
-}

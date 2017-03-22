f93d5f99745b698b267f76b0bc640181bece331c
spanoselias <spanoselias@outlook.com>
2017-03-22 13:19:43 +0000

    modify create sql script

--- Assignment2/createSpatialIndex.sql
+++ Assignment2/createSpatialIndex.sql
@@ -1,15 +1,16 @@
--- Create table if it does not exist.
+/*Create table if it does not exist.*/
 CREATE TABLE IF NOT EXISTS item_coordPoint (
 	item_id INT PRIMARY KEY,
 	coords POINT NOT NULL
 ) ENGINE = MyISAM;
 
---It is inserted all the coordinates and item ids
+/*It is inserted all the coordinates and item ids.*/
+
 INSERT IGNORE INTO item_coordPoint(item_id,coords)
 	SELECT item_id, POINT(latitude,longitude) 
 	FROM item_coordinates;
 
---Create spatial index if it does not exist
+/*Create spatial index if it does not exist.*/
 SELECT IF (
     EXISTS(
         SELECT DISTINCT INDEX_NAME  FROM information_schema.statistics
@@ -18,6 +19,6 @@ SELECT IF (
     )
     ,'SELECT ''index already exists'' Warning;'
     ,'CREATE SPATIAL INDEX sPointIndex on item_coordPoint(coords)') into @a;
-PREPARE stmt1 FROM @a;
-EXECUTE stmt1;
-DEALLOCATE PREPARE stmt1;
+PREPARE stmt FROM @a;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;

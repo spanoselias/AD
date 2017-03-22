f489c739f278c8d91f32e3ad5f436cd357ca6b68
spanoselias <spanoselias@outlook.com>
2017-03-22 14:37:15 +0000

    creation of runLoad.sh

---
+++ Assignment2/runLoad.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+mysql ad < dropSpatialIndex.sql
+
+mysql ad < createSpatialIndex.sql
+
+javac -cp /usr/share/java/mysql-connector-java-5.1.28.jar:/usr/share/java/lucene-core-5.4.0.jar:/usr/share/java/lucene-analyzers-common-5.4.0.jar:/usr/share/java/lucene-queryparser-5.4.0.jar:/usr/share/java/lucene-queries-5.4.0.jar:. Indexer.java
+
+java -cp /usr/share/java/mysql-connector-java-5.1.28.jar:/usr/share/java/lucene-core-5.4.0.jar:/usr/share/java/lucene-analyzers-common-5.4.0.jar:/usr/share/java/lucene-queryparser-5.4.0.jar:/usr/share/java/lucene-queries-5.4.0.jar:. Indexer
+
+javac -cp /usr/share/java/mysql-connector-java-5.1.28.jar:/usr/share/java/lucene-core-5.4.0.jar:/usr/share/java/lucene-analyzers-common-5.4.0.jar:/usr/share/java/lucene-queryparser-5.4.0.jar:/usr/share/java/lucene-queries-5.4.0.jar:. Searcher.java
+
+# We check the number of parameter. If it's one then it means that we should execute
+# the basic search
+if [ "$#" -eq "1" ]
+then 
+java -cp /usr/share/java/mysql-connector-java-5.1.28.jar:/usr/share/java/lucene-core-5.4.0.jar:/usr/share/java/lucene-analyzers-common-5.4.0.jar:/usr/share/java/lucene-queryparser-5.4.0.jar:/usr/share/java/lucene-queries-5.4.0.jar:. Searcher "$1"
+
+# We check the number of parameter. If it's seven then it means that we should execute
+# the spatial search
+elif [ "$#" -eq "7" ]
+then
+java -cp /usr/share/java/mysql-connector-java-5.1.28.jar:/usr/share/java/lucene-core-5.4.0.jar:/usr/share/java/lucene-analyzers-common-5.4.0.jar:/usr/share/java/lucene-queryparser-5.4.0.jar:/usr/share/java/lucene-queries-5.4.0.jar:. Searcher "star trek" -x "$3" -y "$5" -w "$7"
+fi

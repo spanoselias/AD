14ebd42f05e0cac34f3ce53d8ccd25d84c0e9b6c
spanoselias <spanoselias@outlook.com>
2017-03-22 13:10:45 +0000

    modify create sql script

--- Assignment2/createSpatialIndex.sql
+++ Assignment2/createSpatialIndex.sql
@@ -8,5 +8,14 @@ INSERT IGNORE INTO item_coordPoint(item_id,coords)
 	FROM item_coordinates;
 
 
-CREATE spatial INDEX ix_spatial_mytable_pt ON item_coordPoint(coords);
- 
+SELECT IF (
+    EXISTS(
+        SELECT DISTINCT INDEX_NAME  FROM information_schema.statistics
+        WHERE table_schema = 'ad'
+        AND table_name = 'item_coordPoint' AND INDEX_NAME  like 'sPointIndex'
+    )
+    ,'SELECT ''index already exists'' Warning;'
+    ,'CREATE SPATIAL INDEX sPointIndex on item_coordPoint(coords)') into @a;
+PREPARE stmt1 FROM @a;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
